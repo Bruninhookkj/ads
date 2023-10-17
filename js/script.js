48b2fd6a7920a6b80d5cbb72286318872cb908bf
Bruninhookkj <142498586+Bruninhookkj@users.noreply.github.com>
2023-10-17 14:18:01 +0000

    cu

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+    alert ("Ola mundo")
+    function TrocarFrase() {
+        
+        var fraseAtual = document.getElementById("frase");
+var elemento = getElementById
+fraseAtual.className = "h1_2";
+elemento.innerHTML = "funcionou, a frase mudou";
+elemento.style.color = "red";
+elemento.style.fontSize ="25px";
+elemento.style.backgroundColor = "blue";
+ document.body.style.backgroundColor = "yellow"
+ elemento.innerHTML = "<img scr='\img/pingu.png\' />";
+ function trocarClasse() {
+    var trocarClasse
+ }
+    }
+function trocarTexto() {
+    var elemento = document.getElementById ("frase")
+    var nova = document.getElementById("novaFrase").value
+    element.innerHTML = nova
+}
