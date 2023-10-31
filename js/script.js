9c7c3ac0be488ba8652a85e5127b71da162e50c2
Bruninhookkj <142498586+Bruninhookkj@users.noreply.github.com>
2023-10-31 11:10:52 +0000

    a

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
+function mudarFrase() {
+    var elemento = document.getElementById("frase")
+    elemento.innerHTML = "Qualquer frase"
+    elemento.style.color = "red";
+    elemento.style.fontSize = "25px";
+    elemento.style.backgroundColor = "yellow";
+   
+    document.body.style.backgroundColor = "blue";
+    elemento.innerHTML = "<img src=\'img/a.jpg\' />"
+}
+ 
+function trocarClasse() {
+    console.log("chamou trocar classe")
+    var fraseAtual = document.getElementById("frase");
+    fraseAtual.className = "h1_2";
+}  
 
-    alert ("Ola mundo")
-    function TrocarFrase() {
-        
-        var fraseAtual = document.getElementById("frase");
-var elemento = getElementById
-fraseAtual.className = "h1_2";
-elemento.innerHTML = "funcionou, a frase mudou";
-elemento.style.color = "red";
-elemento.style.fontSize ="25px";
-elemento.style.backgroundColor = "blue";
- document.body.style.backgroundColor = "yellow"
- elemento.innerHTML = "<img scr='\img/pingu.png\' />";
- function trocarClasse() {
-    var trocarClasse
- }
-    }
-function trocarTexto() {
-    var elemento = document.getElementById ("frase")
+function trocarTexto(){
+    var elemento = document.getElementById("frase")
     var nova = document.getElementById("novaFrase").value
-    element.innerHTML = nova
+
+    elemento.innerHTML = nova
 }
